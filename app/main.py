0ee183c6556ff61aca788274f90e1fbd36890a95
Faith Meacham <55262704+faithmeacham@users.noreply.github.com>
2020-10-05 09:14:30 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
-from datetime import datetime
 from fastapi import FastAPI
+from fastapi import Request
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return { "time": datetime.now() }
+@app.get("/fibonacci/{length}")
+async def fib(length: int):
+    fib-string = "1"
+    prev = 1
+    current = 1
+    for x in range(length):
+        fib-string += str(current)
+        holder = current
+        current += prev
+        prev = holder
+    return fib-string
